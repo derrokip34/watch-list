d65f54cadc7b32b05764c0b9d54216f9b69b537f
derrickip34 <derrickip34@gmail.com>
2020-04-29 17:53:17 +0300

    flask week 2 content

--- config.py
+++ config.py
@@ -1,21 +1,42 @@
 import os
 
+
 class Config:
     MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://derrick:montolivo@localhost/watchlist'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    SIMPLEMDE_JS_FILE = True
+    SIMPLEMDE_USE_CDN = True
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class ProdConfig(Config):
 
     pass
 
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://derrick:montolivo@localhost/watchlist'
 
 class DevConfig(Config):
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://derrick:montolivo@localhost/watchlist'
     DEBUG = True
 
+
 config_options = {
-'development':DevConfig,
-'production':ProdConfig
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
 }
--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export MOVIE_API_KEY=e699c48f914c566cf9dccd32eac5fe7c
 export SECRET_KEY=derrokip1998
+export MAIL_USERNAME=derrickip34@gmail.com
+export MAIL_PASSWORD=montolivo
 
 python3.6 manage.py server
